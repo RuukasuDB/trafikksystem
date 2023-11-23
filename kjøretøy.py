10efc30822fb307d1aa00561255c221161e2c8ca
Myrstad <mortenmyrstad@gmail.com>
2023-11-23 13:28:32 +0100

    engelsk navn på kjøretøy

--- kjøretøy.py
+++
@@ -1,6 +0,0 @@
-class Kjøretøy(object):
-  def __init__(self, merke, eier, modell, kategori = {"el":False, "bensin":False, "diesel":False}) -> None:
-    self.merke = merke
-    self.eier = eier
-    self.modell = modell
-    self.kategori = kategori
