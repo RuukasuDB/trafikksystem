9e212a596add0ddd3f5e6534a407dd718282eade
Lukas Jasinskas <Lukas.jasinskas1@gmail.com>
2023-11-23 13:40:16 +0100

    engelsk og passeringsklasse

--- passeringklasse.py
+++ passeringklasse.py
@@ -1,5 +1,5 @@
-class Passering():
-    dato = {}
-    time = {}
-    register_numb = {}
-    
+class Pass():
+    def __init__(self, date, time, registration_number):
+        self.date = date
+        self.time = time
+        self.registration_number = registration_number
